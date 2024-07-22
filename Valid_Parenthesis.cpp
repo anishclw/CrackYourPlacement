60b8d5b4ec636863a5677da957156501cd9da4b9
Anish Kumar De <anishclw123@gmail.com>
2024-07-22 20:20:22 +0530

    Added seventh day solutions

---
+++ Valid_Parenthesis.cpp
@@ -0,0 +1,36 @@
+#include <bits/stdc++.h>
+class Solution
+{
+public:
+    bool isValid(string s)
+    {
+        stack<char> st;
+        if (s.length() % 2 != 0)
+            return false;
+        for (int i = 0; i < s.length(); i++)
+        {
+            if (s[i] == '(' || s[i] == '{' || s[i] == '[')
+            {
+                st.push(s[i]);
+            }
+            else
+            {
+                if (st.size() == 0)
+                    return false;
+                char top = st.top();
+                if ((s[i] == ')' && top == '(') ||
+                    (s[i] == '}' && top == '{') ||
+                    (s[i] == ']' && top == '['))
+                {
+                    st.pop();
+                }
+                else
+                    return false;
+            }
+        }
+        if (st.size() == 0)
+            return true;
+        else
+            return false;
+    }
+};
