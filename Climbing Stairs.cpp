93ace9b1a2f0490d828972b17032d2391d61330a
Anish Kumar De <anishclw123@gmail.com>
2024-08-17 10:40:45 +0530

    thirtythree days solutions added

---
+++ Climbing Stairs.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    int foo(int n, vector<int> &dp) {
+        if(n == 1 || n == 0)return 1;
+        if(dp[n] != -1)
+            return dp[n];
+        return dp[n] = foo(n-1, dp) + foo(n-2, dp);
+    }
+    int climbStairs(int n) {
+        vector<int> dp(n+1, -1);
+        int ans = foo(n, dp);
+        return ans;
+    }
+};
